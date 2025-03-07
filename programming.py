7ada8fe738bedfaf90b7da1d71787ddcdb987597
Bjm0114 <superwoofer948@gmail.com>
2025-03-07 16:40:55 +0000

    프로그래밍(1) 연습 완료

--- programming.py
+++ programming.py
@@ -74,6 +74,85 @@ print(e)
 f=a**2 +1
 print(f)
 
+#나눗셈의 몫
 f = 8//b + 3//2
 print(f)
-#오늘은 여기까지 ㅎㅎㅎ
+
+#정수 나눗셈의 나머지: %는 나머지 계산
+g = c%b + f
+print(g)
+
+#몫 연산 vs 나눗셈 연산:정수 vs 부동소수점
+print(c/b)
+
+#나눗셈을 제외하고 정수를 이용한 다른 연산은 기본적으로 정수로 계산된다. 하지만 점(.)을 찍으면 유리수 , 즉 부동소수점으로
+#취급된다. 따라서 모든 연산의 결과가 부동소수점으로 계산된다.
+print(a + 3.)
+print(a - 3.)
+print(a * 3.)
+print(a / 3.)
+
+#단 정수를 가리키는 변수에 점을 찍는것은 허용되지 않는다.
+# d=7
+# a = d. 이런식으로 하면 syntax error 즉 구문 오류가 뜬다.
+
+
+
+
+
+
+
+
+#여기서부터는 예제
+# print("Hello World" -- >  괄호가 하나 빠짐 (닫는 괄호)
+# print('Hello World") -- > 작은 따옴표로 열때는 작은 따옴표로 닫아야함
+
+print(+2) #----> 오류가 발생하지 않음 +2는 그냥 2로 본다.
+
+# print(023) --> 부동소수점이나 정수 등은 0으로 시작할 수 없다.
+print('023') #--> 이와같이 ''를 이용하여 '023'을 나타낼 수 있다.
+
+#예제 2 변수할당 명령문과 관련한 주의사항
+#23 = n ----> 변수 할당 명령문의 등호 기호 왼쪽에 변수가 와야한다.
+
+x=y=1
+# ---> 오류발생하지 않음 하지만 줄바꿔쓰는거 추천
+
+# y= k+1
+# print(y) # 변수 k가 지정되지 않음
+
+k=2
+y= k+1
+print(y) #이런식으로 해야 정상적으로 출력됨
+
+#예제 3
+number = 17
+print(number + 2)
+print(number - 2.0)
+print(number / 7)
+print(number //7)
+print(number % 7)
+print(number**3 / (2+3))
+print(number**3 / 2+3)
+
+#예제 4
+
+print(3 + 3.0)
+#print(3 + '3.0') --- > 따옴표 씌우면 오류발생
+print(3 * '3.0')
+
+#예제 5
+#1m**2은 0.3025평이다. 84m**2은 몇평인가? 반대로 30평은 몇 m**2인지 계산하는 코드를 다양한 변수를 이용하여 구한다.
+
+#one_squaremeter: 1제곱미터에 해당하는 평수
+#eightyfour_squaremeter: 84제곱미터에 해당하는 평수
+##one_pyeong: 1평에 해당하는 제곱미터
+#thirty_pyeong: 30평에 해당하는 제곱미터
+
+one_squaremeter = 0.3025
+eightyfour_squaremeter =  84 * one_squaremeter
+one_pyeong = 1 / one_squaremeter
+thirty_pyeong = 30 * one_pyeong
+
+print(eightyfour_squaremeter, "평")
+print(thirty_pyeong, "제곱미터")
